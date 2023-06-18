178d3d960febe7decf6f31fd89ad8ae7dace7c61
Wout <hi@wout.codes>
2023-06-18 11:10:30 +0200

    chore: add favicon

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -41,5 +41,18 @@ export default defineConfig({
       { icon: 'discord', link: 'https://discord.com/invite/64ct9tQzgn' },
       { icon: 'github', link: 'https://github.com/venster-io' }
     ]
-  }
+  },
+
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['link', { rel: 'icon', href: '/favicon-16x16.png', type: 'image/png', sizes: '16x16' }],
+    ['link', { rel: 'icon', href: '/favicon-32x32.png', type: 'image/png', sizes: '32x32' }],
+    ['link', { rel: 'apple-touch-icon', href: '/apple-touch-icon.png', sizes: '180x180' }],
+    ['link', { rel: 'mask-icon', href: 'safari-pinned-tab.svg', color: '#363c31' }],
+    ['link', { rel: 'manifest', href: '/site.webmanifest' }],
+    ['meta', { name: 'apple-mobile-web-app-title', content: 'venster' }],
+    ['meta', { name: 'application-name', content: 'venster' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#ffffff' }],
+    ['meta', { name: 'theme-color', content: '#ffffff' }]
+  ]
 })
